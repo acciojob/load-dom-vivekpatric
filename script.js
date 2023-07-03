ea46fde043a417de96f94773554f8589d1a7e775
Vivek Patric R <96299357+vivekpatric@users.noreply.github.com>
2023-07-03 20:27:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+function onDOMLoad() {
+        document.body.textContent = "DOM load success";
+      }
+      document.addEventListener("DOMContentLoaded", onDOMLoad);
